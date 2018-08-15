158379176b2ca59c94064b2cc8d02cae23724549
JellyBool <jellybool@outlook.com>
2018-08-15 17:23:33 +0800

    add queue

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Mysql Laravel
+                    Queue Laravel
                 </div>
 
                 <div class="links">
