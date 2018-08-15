b852eefdce007ed1ed676bc73f5ef10162fa20b4
JellyBool <jellybool@outlook.com>
2018-08-15 11:47:51 +0800

    add mysql support

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Deploy Laravel
+                    Mysql Laravel
                 </div>
 
                 <div class="links">
