53abcfff9b89098c432910e7b6db71b01dc352a6
JellyBool <jellybool@outlook.com>
2018-08-16 17:57:22 +0800

    add cron job

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,6 +26,8 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
          $schedule->command('log:demo')
+                  ->withoutOverlapping()
+                  ->onOneServer()
                   ->everyMinute();
     }
 
