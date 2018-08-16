0f97d72211ccce7e3a7657192ffcfa1f75b3371a
JellyBool <jellybool@outlook.com>
2018-08-16 17:55:31 +0800

    add cron job

---
+++ app/Console/Commands/LogDemo.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Task;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+
+class LogDemo extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'log:demo';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Log Demo';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        Task::create(['body' => Carbon::now()->timestamp]);
+    }
+}
